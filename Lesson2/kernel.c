cb2f918e4418b91accf8e335f4b43df77b978815
john-liu2039 <jl@ad2039.com>
2021-08-29 21:46:33 -0500

    simulation RPI3 working

--- Lesson2/kernel.c
+++ Lesson2/kernel.c
@@ -11,47 +11,47 @@ void main()
     uart_init();
     fb_init();
 
-    // drawRect(150,150,400,400,0x03,0);
-    // drawRect(300,300,350,350,0x2e,1);
+    drawRect(150,150,400,400,0x03,0);
+    drawRect(300,300,350,350,0x2e,1);
 
-    // drawCircle(960,540,250,0x0e,0);
-    // drawCircle(960,540,50,0x13,1);
+    drawCircle(960,540,250,0x0e,0);
+    drawCircle(960,540,50,0x13,1);
 
-    // drawPixel(250,250,0x0e);
+    drawPixel(250,250,0x0e);
 
-    // drawChar('O',500,500,0x05);
-//    drawString(x_oft, y_oft,"Hello world!",0x0f);
-    //y_oft += FONT_HEIGHT; 
-    //uart_writeText("Hello Word!\n");
+    drawChar('O',500,500,0x05);
+    drawString(x_oft, y_oft,"Hello world!",0x0f);
+    y_oft += FONT_HEIGHT; 
     char msg0 = 't';
     mmio_write(extern_AUX_MU_IO_REG, (unsigned int)msg0);
     //while(1) uart_update();
 
     //char msg0 = 't';
     //uart_send(extern_AUX_MU_IO_REG, msg0);
-    // drawLine(100,500,350,700,0x0c);
-    // char msg;
-    // while (1){
-    //     if(x_oft >= 1920) {
-    //         x_oft = 0;
-    //         y_oft += FONT_HEIGHT;
-    //     }
-    //     if(y_oft >= 1080) {
-    //         y_oft = 0;
-    //         x_oft = 0;
-    //     }
-    //     msg = uart_readByte();
-    //     drawChar(msg,x_oft, y_oft, 0x0f);
-    //     //uart_writeText(&msg);
-    //     //uart_send(extern_AUX_MU_IO_REG, msg);
-    //     if(msg=='\n' || msg == '\r') {
-    //         x_oft = 0;
-    //         y_oft += (FONT_HEIGHT + 2);
+    drawLine(100,500,350,700,0x0c);
+    char msg;
+    while (1){
+        if(x_oft >= 1920) {
+            x_oft = 0;
+            y_oft += FONT_HEIGHT;
+        }
+        if(y_oft >= 1080) {
+            y_oft = 0;
+            x_oft = 0;
+        }
+        msg = uart_readByte();
+        drawChar(msg,x_oft, y_oft, 0x0f);
+        mmio_write(extern_AUX_MU_IO_REG, (unsigned int)msg);
+        //uart_writeText(&msg);
+        //uart_send(extern_AUX_MU_IO_REG, msg);
+        if(msg=='\n' || msg == '\r') {
+            x_oft = 0;
+            y_oft += (FONT_HEIGHT + 2);
 
-    //     }
-    //     else{
-    //         x_oft += (FONT_WIDTH + 1);
-    //     }
+        }
+        else{
+            x_oft += (FONT_WIDTH + 1);
+        }
 
-    // }
+    }
 }
